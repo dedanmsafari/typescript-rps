a6dcc7ad58adf6cb3a60c6569323b2f959f65144
dedanmsafari <dedan.msafari@gmail.com>
2023-10-29 08:54:55 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.titleCtn}>
+          <h1>ROCK,PAPER,SCISSORS</h1>
+          <p>React Typescript Game!</p>
+        </div>
+        {/* <ScoreAndResults />
+        <ChooseAndPlay /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
