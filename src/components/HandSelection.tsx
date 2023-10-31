ee7848a293623142adacd65b274a1d9a60d8b122
dedanmsafari <dedan.msafari@gmail.com>
2023-10-31 07:30:23 +0300

    MODIFIED HAND SELECTION

--- src/components/HandSelection.tsx
+++ src/components/HandSelection.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+
 import styles from "./HandSelection.module.css";
+import Button from "../utils/component/Button.component";
 
 type HandProps = {
   name: string;
   icon: React.JSX.Element;
+  onClick: () => void;
 };
-
-const HandSelection = ({ name, icon }: HandProps) => {
+//console.log("I am HAND SELECTION");
+const HandSelection = ({ name, icon, onClick }: HandProps) => {
   return (
-    <button className={styles.choiceBtn}>
-      {name} {icon}
-    </button>
+    <Button
+      name={name}
+      icon={icon}
+      className={styles.choiceBtn}
+      onClick={onClick}
+    />
   );
 };
 
