f55539f4088a0130cf5d637571686cae4ae2833e
dedanmsafari <dedan.msafari@gmail.com>
2023-11-15 08:43:55 +0300

    Option Actions

--- src/actions/OptionActions.ts
+++ src/actions/OptionActions.ts
@@ -1,4 +1,5 @@
 import { HandOptions } from "../models/Options";
+import RandomValue from "../utils/functions/randomVal";
 
 type Action<T = string> = {
   type: T;
@@ -20,15 +21,28 @@ export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
 }
 
-export const RockOption = createAction(HandOptions.rock, {
+export const RockOption = createAction(HandOptions.rock as HandOptions.rock, {
   playerHand: 0,
   runtime: true,
 });
-export const PaperOption = createAction(HandOptions.paper, {
-  playerHand: 1,
-  runtime: true,
-});
-export const ScissorsOption = createAction(HandOptions.scissors, {
-  playerHand: 2,
-  runtime: true,
-});
+export const PaperOption = createAction(
+  HandOptions.paper as HandOptions.paper,
+  {
+    playerHand: 1,
+    runtime: true,
+  }
+);
+export const ScissorsOption = createAction(
+  HandOptions.scissors as HandOptions.scissors,
+  {
+    playerHand: 2,
+    runtime: true,
+  }
+);
+export const ComputerOption = createAction(
+  HandOptions.computer as HandOptions.computer,
+  () => ({
+    computerHand: RandomValue(),
+    runtime: true,
+  })
+);
