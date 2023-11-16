cc1897eb34fc86cd7443ff2de7001ebb5f80600e
dedanmsafari <dedan.msafari@gmail.com>
2023-11-16 09:21:29 +0300

    options reducer updated

--- src/reducers/optionsReducer.ts
+++ src/reducers/optionsReducer.ts
@@ -40,11 +40,50 @@ export const optionsReducer: OptionsReducerType = (state, action) => {
       runTimer: action.payload.runtimer,
     };
   }
+  if (action.type === HandOptions.computerWins) {
+    return {
+      ...state,
+      score: {
+        player: state.score.player + action.payload.player,
+        computer: state.score.computer + action.payload.computer,
+      },
+      results: {
+        winner: action.payload.winner,
+        message: action.payload.message,
+      },
+    };
+  }
+  if (action.type === HandOptions.playerWins) {
+    return {
+      ...state,
+      score: {
+        player: state.score.player + action.payload.player,
+        computer: state.score.computer + action.payload.computer,
+      },
+      results: {
+        winner: action.payload.winner,
+        message: action.payload.message,
+      },
+    };
+  }
+  if (action.type === HandOptions.draw) {
+    return {
+      ...state,
+      score: {
+        player: state.score.player + action.payload.player,
+        computer: state.score.computer + action.payload.computer,
+      },
+      results: {
+        winner: action.payload.winner,
+        message: action.payload.message,
+      },
+    };
+  }
   return {
     ...state,
     results: {
       winner: "error",
-      message: "We have an Error processing Options reducer",
+      message: "Internal Error,Computer is hurt :cry:",
     },
   };
 };
