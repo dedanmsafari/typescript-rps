83d4e4a6194eb0b38d03280b363e3c73d4d3e097
dedanmsafari <dedan.msafari@gmail.com>
2023-11-16 09:21:09 +0300

    additional option actions

--- src/actions/OptionActions.ts
+++ src/actions/OptionActions.ts
@@ -49,3 +49,28 @@ export const ComputerOption = createAction(
     runtimer: true,
   })
 );
+
+export const ComputerWinsOption = createAction(
+  HandOptions.computerWins as HandOptions.computerWins,
+  {
+    winner: "Computer",
+    message: "AI defeated Human,Mwahahahaha!",
+    computer: 1,
+    player: 0,
+  }
+);
+export const PlayerWinsOption = createAction(
+  HandOptions.playerWins as HandOptions.playerWins,
+  {
+    winner: "Player",
+    message: "Human defeated AI,There is still hope left",
+    computer: 0,
+    player: 1,
+  }
+);
+export const DrawOption = createAction(HandOptions.draw as HandOptions.draw, {
+  winner: "Draw",
+  message: "What a Draw.Play Again!",
+  computer: 0.5,
+  player: 0.5,
+});
