5f4048fdd1ac468ee236ee7c32a37ddb602ca160
dedanmsafari <dedan.msafari@gmail.com>
2023-11-21 05:24:31 +0300

    score and results test

--- src/sections/ScoreAndResults.test.tsx
+++ src/sections/ScoreAndResults.test.tsx
@@ -4,6 +4,12 @@ import ChooseAndPlay from "./ChooseAndPlay";
 import ScoreAndResults from "./ScoreAndResults";
 import OptionsProvider from "../context/options.context";
 
+vi.mock("../utils/functions/randomVal", () => {
+  return {
+    randomValue: () => 0,
+  };
+});
+
 describe("Score And Result", () => {
   it("Should display 1 second after elapsed time of 2 seconds on the timer", () => {
     vi.useFakeTimers();
@@ -33,8 +39,8 @@ describe("Score And Result", () => {
     vi.useFakeTimers();
     render(
       <OptionsProvider>
-        <ChooseAndPlay />
         <ScoreAndResults />
+        <ChooseAndPlay />
       </OptionsProvider>
     );
 
@@ -52,5 +58,119 @@ describe("Score And Result", () => {
     });
 
     expect(screen.getByTestId("timer")).toHaveTextContent("2");
+  });
+
+  it("Should display the Player Winner after timer goes off in 3 seconds", () => {
+    vi.useFakeTimers();
+    render(
+      <OptionsProvider>
+        <ScoreAndResults />
+        <ChooseAndPlay />
+      </OptionsProvider>
+    );
+
+    const paper = screen.getByText(/paper/i);
+
+    const play = screen.getByText("play");
+
+    expect(paper).toBeInTheDocument();
+
+    expect(play).toBeInTheDocument();
+
+    fireEvent.click(paper);
+    fireEvent.click(play);
+
+    act(() => {
+      vi.advanceTimersByTime(3000);
+    });
+
+    expect(screen.getByText("Player Wins!")).toBeInTheDocument();
+    expect(
+      screen.getByText("There is still hope left for Humanity!")
+    ).toBeInTheDocument();
+    screen.debug();
+
+    expect(screen.getByText(/Player: 1/)).toBeInTheDocument();
+    expect(screen.getByText(/Computer: 0/)).toBeInTheDocument();
+
+    expect(screen.getAllByTestId(/rock/i)[0]).toBeVisible();
+    expect(screen.getAllByTestId(/paper/i)[0]).toBeVisible();
+
+    expect(screen.getAllByTestId(/rock/i)).toHaveLength(2);
+  });
+
+  it("Should display the Computer Winner after timer goes off in 3 seconds", () => {
+    vi.useFakeTimers();
+    render(
+      <OptionsProvider>
+        <ScoreAndResults />
+        <ChooseAndPlay />
+      </OptionsProvider>
+    );
+
+    const scissors = screen.getByText(/scissors/i);
+
+    const play = screen.getByText("play");
+
+    expect(scissors).toBeInTheDocument();
+
+    expect(play).toBeInTheDocument();
+
+    fireEvent.click(scissors);
+    fireEvent.click(play);
+
+    act(() => {
+      vi.advanceTimersByTime(3000);
+    });
+
+    expect(screen.getByText("Computer Wins!")).toBeInTheDocument();
+    expect(
+      screen.getByText("AI will rule you one day! ~ Computer")
+    ).toBeInTheDocument();
+    screen.debug();
+
+    expect(screen.getByText(/Player: 0/)).toBeInTheDocument();
+    expect(screen.getByText(/Computer: 1/)).toBeInTheDocument();
+
+    expect(screen.getAllByTestId(/rock/i)[0]).toBeVisible();
+    expect(screen.getAllByTestId(/scissors/i)[0]).toBeVisible();
+
+    expect(screen.getAllByTestId(/scissors/i)).toHaveLength(2);
+  });
+  it("Should display a Draw after timer goes off in 3 seconds", () => {
+    vi.useFakeTimers();
+    render(
+      <OptionsProvider>
+        <ScoreAndResults />
+        <ChooseAndPlay />
+      </OptionsProvider>
+    );
+
+    const rock = screen.getByText(/rock/i);
+
+    const play = screen.getByText("play");
+
+    expect(rock).toBeInTheDocument();
+
+    expect(play).toBeInTheDocument();
+
+    fireEvent.click(rock);
+    fireEvent.click(play);
+
+    act(() => {
+      vi.advanceTimersByTime(3000);
+    });
+
+    expect(screen.getByText("Draw")).toBeInTheDocument();
+    expect(screen.getByText("What a Draw. Play Again!")).toBeInTheDocument();
+    screen.debug();
+
+    expect(screen.getByText(/Player: 0.5/)).toBeInTheDocument();
+    expect(screen.getByText(/Computer: 0.5/)).toBeInTheDocument();
+
+    expect(screen.getAllByTestId(/rock/i)[0]).toBeVisible();
+    expect(screen.getAllByTestId(/rock/i)[0]).toBeVisible();
+
+    expect(screen.getAllByTestId(/rock/i)).toHaveLength(3);
   });
 });
